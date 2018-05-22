80aa8832c306801f6782b0ef2df85c9901f40503
Albert Sare <thesundaymailman@gmail.com>
2018-05-22 10:33:57 -0700

    refined day 1 solutions

--- server.js
+++ server.js
@@ -12,17 +12,24 @@ app.use(express.static('public'));
 // http://127.0.0.1:8080/api/notes?searchTerm=cats this is an example query
 app.get('/api/notes/', (req, res) => {
   const searchTerm = req.query.searchTerm;
-  const foundData = data.filter(item => item.title.includes(searchTerm));
-  res.json(foundData);
-});
+  if(searchTerm) {
+    const foundData = data.filter(item => item.title.includes(searchTerm));
+    res.json(foundData);
+  } else {
+    res.json(data);
+  }
 
+  // this is the short-hand method of the above
+  // const {searchTerm} = req.query;
+  // res.json(searchTerm ? data.filter(item => item.title.includes(searchTerm)) : data);
+});
 
 // GET /api/notes/:id returns a specific note based on the ID provided.
+// http://127.0.0.1:8080/api/1005 this is an example of id search endpoint
 app.get('/api/notes/:id', (req, res) => {
-  const foundData = data.find(item => item.id === Number(req.params.id));
-  res.json(foundData);
+  const foundId = data.find(item => item.id === Number(req.params.id));
+  res.json(foundId);
 });
-
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
