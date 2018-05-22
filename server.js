1b84972d9d872ca86b2fd983ce2bf13e84f076f8
Albert Sare <thesundaymailman@gmail.com>
2018-05-21 22:26:03 -0700

    finished exercises for 05-21-18

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
 'use strict';
 
-// Load array of notes
+const express = require('express');
+const app = express();
+
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
+// ADD STATIC SERVER HERE
+app.use(express.static('public'));
 
-// INSERT EXPRESS APP CODE HERE...
+// GET function to support search query
+// http://127.0.0.1:8080/api/notes?searchTerm=cats this is an example query
+app.get('/api/notes/', (req, res) => {
+  const searchTerm = req.query.searchTerm;
+  const foundData = data.filter(item => item.title.includes(searchTerm));
+  res.json(foundData);
+});
+
+
+// GET /api/notes/:id returns a specific note based on the ID provided.
+app.get('/api/notes/:id', (req, res) => {
+  const foundData = data.find(item => item.id === Number(req.params.id));
+  res.json(foundData);
+});
+
+
+app.listen(8080, function () {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
